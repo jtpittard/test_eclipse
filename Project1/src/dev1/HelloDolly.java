35d4d427cfbbac5189f9fc501ec3eb981cf4b396
John Pittard <about28rocks@gmail.com>
2018-08-21 20:57:43 -0400

    Made conversation

--- Project1/src/dev1/HelloDolly.java
+++ Project1/src/dev1/HelloDolly.java
@@ -3,7 +3,7 @@ package dev1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Helow Werld");
 		HelloWorld.main(null);
+		System.out.println("Not much, You?");
 	}
 }
